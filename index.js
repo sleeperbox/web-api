55cd21fde83fd2efaf372a1b86363f62ef3717f0
deaaprizal <dea.aprizal@icloud.com>
2018-12-13 01:11:41 +0700

    api button and category

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
-const express = require('express')
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const mongoose = require('mongoose')
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost:27017/way')
-mongoose.Promise = global.Promise
+mongoose.connect("mongodb://localhost:27017/way");
+mongoose.Promise = global.Promise;
 
-const app =  express()
+const app = express();
 
-app.use('/api', require('./routes/user'))
-app.use('/api', require('./routes/bypass'))
-app.use(morgan('combine'))
-app.use(cors())
-app.use(bodyParser.urlencoded({
+app.use("/api", require("./routes/user"));
+app.use("/api", require("./routes/bypass"));
+app.use("/api/profile", require("./routes/profile/button-menu"));
+app.use("/api/profile", require("./routes/profile/more-category"));
+
+app.use(morgan("combine"));
+app.use(cors());
+app.use(
+  bodyParser.urlencoded({
     extended: false
-  }))
+  })
+);
 
-app.use(bodyParser.json({ type: 'application/*+json' }))
+app.use(bodyParser.json({ type: "application/*+json" }));
 
-app.get('/', (req, res) => {
-    console.log('ok getting / ')
-        res.send([{
-            "msg": "sukses"
-    }]);
-})
+app.get("/", (req, res) => {
+  console.log("ok getting / ");
+  res.send([
+    {
+      msg: "sukses"
+    }
+  ]);
+});
 
-app.listen(process.env.PORT || 8080, function(){
-    console.log('starting server 8080...')
-})
+app.listen(process.env.PORT || 8080, function() {
+  console.log("starting server 8080...");
+});
